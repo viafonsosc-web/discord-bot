a687e10e766d15faa88b6ce074835d246bb9f5ad
viafonsosc-web <viafonsosc@gmail.com>
2026-03-07 14:42:52 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -174,7 +174,8 @@ import os
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
-bot.run(os.getenv("DISCORD_TOKEN"))
 
+# Pega o token da variável de ambiente
+bot.run(os.getenv("DISCORD_TOKEN"))
 
 
